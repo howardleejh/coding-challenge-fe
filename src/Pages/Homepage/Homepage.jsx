cbeb07051015c70175b25c806d768899e936137e
howardleejh <vault2howard@gmail.com>
2022-09-29 20:37:28 +0800

    added side effect to check if server is ready for API

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,8 +1,34 @@
-import { Row } from 'antd'
+import { useEffect } from 'react'
+import { Row, message } from 'antd'
 import Welcome from '../../Components/Welcome/Welcome'
+import axios from 'axios'
 import './Homepage.scss'
 
 const Homepage = () => {
+  useEffect(() => {
+    const checkServer = async () => {
+      let response
+      try {
+        response = await axios({
+          method: 'get',
+          url: `${process.env.REACT_APP_BE}/`,
+        })
+        if (response.status !== 200) {
+          message.error(`Waiting for server.`, 5)
+          checkServer()
+          return
+        }
+        message.success(response.data)
+        return
+      } catch (err) {
+        message.error(`${err.response.data}`)
+        return
+      }
+    }
+
+    checkServer()
+  }, [])
+
   return (
     <div className='homepage'>
       <Row align='middle' justify='center'>
